2544f02a1e6925809f7365fd8d25e8ec70497ad9
Xin Yi Li <61605285+lixin-source@users.noreply.github.com>
2023-08-24 20:39:51 -0400

    Added navbar & about

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,74 @@
+import { useEffect } from 'react';
 import './App.css'
 import Type from "./components/Type";
 
 function App() {
   
+  useEffect(() => {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+      link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(event) {
+      event.preventDefault();
+      const targetId = event.currentTarget.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      // Calculate the offset based on the height of the navigation bar
+      const navHeight = document.querySelector('nav').offsetHeight;
+      const targetPosition = targetElement.offsetTop - navHeight;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+
+    // Cleanup event listeners
+    return () => {
+      navLinks.forEach(link => {
+        link.removeEventListener('click', smoothScroll);
+      });
+    };
+  }, []);
+
   return (
     <div>
-      <h2> Hello world!</h2>
+    {/* NAV */}
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#experiences">Experiences</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+    {/* HOME */}
+    <section id="home">
       <Type/>
+    </section>
+
+    {/* ABOUT */}
+    <section id="about">
+      <div className='aboutText'>
+      <p> Hi! I&apos;m Xin Yi Li, a first-generation college graduate from Brooklyn College in May 2023 (Computer Science & Sociology).
+        I&apos;ve participated in tech opportunities with Microsoft Research, Pfizer, Brooklyn College&apos;s Cancer Center, and more!
+      </p>
+      <p> I also have a strong skillset in public speaking, content creation, social media management, and community organizing that I developed
+        during my undergrad with NYC&apos;s Department of Education, CUNY, TheDream.US, and more!
+      </p>
+      <p>
+        The main programming languages I use are Python & Javascript.
+      </p>
+      <p> I&apos;m open to opportunities in frontend development, data science/analysis, UX research, community organizing, and public speaking. Feel free to reach out!</p>
+      </div>
+      <img src='src/imgs/aboutimg.png' className='aboutimg'/>
+    </section>
+
+    <section id="experiences">
+      
+    </section>
     </div>
   )
 }
--- portfolio/src/components/Type.jsx
+++ portfolio/src/components/Type.jsx
@@ -1,20 +1,30 @@
 import Typewriter from "typewriter-effect";
+import "../App.css"; // Assuming you have a CSS file for styling
 
 function Type() {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "UX/UI Designer",
-          "Web Developer",
-          "Data Scientist",
-          "Advocate",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+    <div className="homeGreet">
+      <h2 className="homeGreetWelcome">Welcome! 👋🏻</h2>
+      <h2 className="homeGreetStatic">
+        I&apos;m{" "}
+        <span className="typewriter-container">
+          <Typewriter
+            options={{
+              strings: [
+                "Xin Yi Li",
+                "a Web Developer",
+                "a UX/UI Designer",
+                "a Data Scientist",
+                "an Advocate",
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 100,
+            }}
+          />
+        </span>
+      </h2>
+    </div>
   );
 }
 
