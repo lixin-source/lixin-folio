b177716542e4ffaa867bab1bfc8bdbf82e8894a7
Xin Yi Li <61605285+lixin-source@users.noreply.github.com>
2023-08-28 23:32:39 -0400

    added curly braces

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,9 @@
+import loader from "../assets/imgs/spin1.gif";
+
 const Loading = () => {
     return (
       <div className="loading">
-        <img src='../assets/imgs/spin1.gif'/>
+        <img src={loader}/>
         <h1>Loading...</h1>
       </div>
     );
